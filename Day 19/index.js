08a68ed56d69c7063f2a502c0ae797e61eb96955
GUNA SGS <96234417+gunasgs@users.noreply.github.com>
2022-01-30 22:16:18 +0530

    calculater.js

--- Day 19/index.js
+++ Day 19/index.js
@@ -1,27 +1,28 @@
-let display = document.getElementById('display');
-
-let buttons = Array.from(document.getElementsByClassName('button'));
-
-buttons.map( button => {
-    button.addEventListener('click', (e) => {
-        switch(e.target.innerText){
-            case 'C':
-                display.innerText = '';
-                break;
-            case '=':
-                try{
-                    display.innerText = eval(display.innerText);
-                } catch {
-                    display.innerText = "Error"
-                }
-                break;
-            case '←':
-                if (display.innerText){
-                   display.innerText = display.innerText.slice(0, -1);
-                }
-                break;
-            default:
-                display.innerText += e.target.innerText;
-        }
-    });
-});
+const calci = document.createElement("section");
+ 
+calci.innerHTML = `
+<div class="container">
+  <div id="display"></div>
+     <div class="buttons">
+     <div class="button" style="color:red">C</div>
+     <div class="button ">←</div>
+     <div class="button math">.</div>
+     <div class="button math">×</div>
+     <div class="button">7</div>
+     <div class="button">8</div>
+     <div class="button">9</div>
+     <div class="button math">/</div>
+     <div class="button">4</div>
+     <div class="button">5</div>
+     <div class="button">6</div>
+     <div class="button math">–</div>
+     <div class="button">1</div>
+     <div class="button">2</div>
+     <div class="button">3</div>
+     <div class="button math">+</div>
+     <div class="button">0</div>
+     <div class="button">00</div>
+     <div id="equal" class="button equal">=</div>
+ </div>
+</div>`
+document.body.appendChild(calci);
